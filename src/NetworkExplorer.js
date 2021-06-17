a5fad52ccc9647e3867ea3f2a5aad9830c16da21
Tom Davidson <thomas.davidson@ibm.com>
2021-06-17 09:59:52 +0100

    adding better network explorer

--- src/NetworkExplorer.js
+++ src/NetworkExplorer.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { ResponsiveNetworkCanvas } from '@nivo/network'
-// import { linkHorizontal, linkVertical } from 'd3-shape';
+import NetworkFrame from "semiotic/lib/NetworkFrame"
 
-let tree =[];
-let links = [];
+let tree = [];
 
-const stylingForDepth = (depth) => {
-  if (depth === 1) return {"radius": 12, "depth": depth, "color": "#ff0000"}
-  if (depth === 2) return {"radius": 8, "depth": depth, "color": "#00ff00"}
-  if (depth === 3) return {"radius": 6, "depth": depth, "color": "#0000ff"}
-  if (depth === 4) return {"radius": 4, "depth": depth, "color": "pink"}
-  if (depth === 5) return {"radius": 3, "depth": depth, "color": "#ffbb00"}
+function checkIfArrayContainsNode(arr, name, parent) {
+  return arr.some(function(arrVal) {
+    return name === arrVal.name && arrVal.parentId === parent;
+  });
+}
+
+function checkIfChildArrContainsNode(arr, name) {
+  return arr.some(function(arrVal) {
+    return name === arrVal.name;
+  });
 }
 
 const generateTopicNameFrom = (messageTopic,index) => {
@@ -19,56 +21,85 @@ const generateTopicNameFrom = (messageTopic,index) => {
   return splitArray.join('/')
 };
 
-function checkIfArrayContainsNode(arr, id) {
-  return arr.some(function(arrVal) {
-    return id === arrVal.id;
-  });
-}
  /*eslint-disable*/
-const addNodesToTree = (messages) => {
+const enrichDataForTree = (messages) => {
   Object.keys(messages).map(messageTopic => {
-    const arrayOfTopicLevels = messageTopic.split('/'); 
-    arrayOfTopicLevels.map((singleNode, index) => {  
-      if (!checkIfArrayContainsNode(tree,generateTopicNameFrom(messageTopic, index))) {
-        tree.push({
-          "id": generateTopicNameFrom(messageTopic, index),
-          ...stylingForDepth(index +2)
-        })
-        links.push({
-          "source": index === 0 ? 'root' : generateTopicNameFrom(messageTopic, index -1),
-          "target": generateTopicNameFrom(messageTopic, index),
-          "distance": 30
-        })
+    const arrayOfTopicLevels = messageTopic.split('/');
+    arrayOfTopicLevels.map((singleNode, index) => {
+      
+      const parentIdToUse = index === 0 ? 'broker' : generateTopicNameFrom(messageTopic, index -1);
+      const contentToUse = index === arrayOfTopicLevels.length - 1 ? messages[messageTopic]: null;
+
+      const idToUse = parentIdToUse !== 'broker' ? (index === messageTopic.split('/').length -1 ? messageTopic : generateTopicNameFrom(messageTopic, index)) : messageTopic.split('/')[index];
+
+      if (!checkIfArrayContainsNode(tree,idToUse,parentIdToUse)) {
+        contentToUse ? tree.push({name: idToUse , displayName: messageTopic.split('/')[index], parentId: parentIdToUse, attributes: {payload: contentToUse}}) : tree.push({name: idToUse , displayName: messageTopic.split('/')[index], parentId: parentIdToUse})
       }
-    }) 
     })
+  })
+  tree.push({name: 'broker' , displayName: 'broker', parentId: null, content: null})
 };
  /*eslint-enable*/
+
+ 
+const generateTree = (data) => {
+  const idMapping = data.reduce((acc, el, i) => {
+    acc[el.name] = i;
+    return acc;
+  }, {});
+
+
+  let root;
+  data.forEach(el => {
+    // Handle the root element
+    if (el.parentId === null) {
+      root = el;
+      return;
+    }
+    // Use our mapping to locate the parent element in our data array
+    const parentEl = data[idMapping[el.parentId]];
+     //Add our current el to its parent's `children` array
+     if (parentEl.children) {
+      if (!checkIfChildArrContainsNode(parentEl.children,el.name)) {
+        parentEl.children = [...(parentEl.children || []), el]
+       }
+     } else {
+        parentEl.children = [...[], el]
+     }
+      
+  });
+  return root;
+}
 
 
 
 
 const buildTree = (messages) => {
-  addNodesToTree(messages);
-  return tree
+  enrichDataForTree(messages);
+  const generatedTree = generateTree(tree);
+  return generatedTree
 };
+
+const frameProps = { 
+    networkType: { type: "force", forceManyBody: -250, distanceMax: 500, edgeStrength: 2 },
+    nodeIDAccessor: function(e){return e.hierarchicalID||e.name},
+    edgeStyle: { stroke: "#9fd0cb", fill: "none" },
+    nodeSizeAccessor: 2
+  }
 
 const NetworkExplorer = ({messages}) => {
   const dataForTree = buildTree(messages);
+  console.log(dataForTree)
   return <div style={{height: 800, border: '5px solid black', margin: '5rem'}}>
-    <ResponsiveNetworkCanvas
-        nodes={[...dataForTree, {id: 'root' ,  ...stylingForDepth(1)}]}
-        links={links}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        linkDistance="distance"
-        repulsivity={37}
-        iterations={60}
-        nodeColor={function(e){return e.color}}
-        nodeBorderWidth={1}
-        nodeBorderColor={{ theme: 'background' }}
-        linkColor="black"
-        animate={false}
-        tooltip={(node)=><span>{JSON.stringify(node)}</span>}
+    <NetworkFrame 
+      edges={dataForTree}  
+      hoverAnnotation={true}
+      tooltipContent={d => (
+      <div className="tooltip-content">
+        <p>Name: {d.name}</p>
+      </div>
+    )} 
+    {...frameProps} 
     />
   </div>;
 }
