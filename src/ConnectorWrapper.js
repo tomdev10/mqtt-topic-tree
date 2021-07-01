865ac196a6ff3d9ccfcc13e5a12f8dbef7f9d155
Tom Davidson <thomas.davidson@ibm.com>
2021-07-01 13:21:56 +0100

    testing avoid reject

--- src/ConnectorWrapper.js
+++ src/ConnectorWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Connector } from 'mqtt-react-hooks';
 
 const ConnectorWrapper = ({children, brokerUrlToUse, clientId, username, password}) => {
-  return brokerUrlToUse ? <Connector brokerUrl={brokerUrlToUse} options={{keepalive: 0, clientId, username, password}}>{children}</Connector> : <>{children}</>;
+  return brokerUrlToUse ? <Connector brokerUrl={brokerUrlToUse} options={{keepalive: 0, clientId, username, password, rejectUnauthorized: false}}>{children}</Connector> : <>{children}</>;
 }
 
 export default ConnectorWrapper;
